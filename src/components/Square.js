d4b47125953082439ba1ddebe651aba23c1e84f9
Alex Belke <alex.belke.de@gmail.com>
2020-05-28 11:42:19 +0200

    refactor: code format

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-const Square = props => {
-    return (
-        <button className="square" onClick={props.onClick}>
-            {props.value}
-        </button>
-    );
-}
+const Square = (props) => {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+};
 
 export default Square;
